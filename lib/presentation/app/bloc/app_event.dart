9a0cab3488d8339209b126dfa6b86fd2c800fde5
yunwe <sawyunwe.ucsy@gmail.com>
2024-05-09 15:41:45 +0700

    added app bloc

---
+++ lib/presentation/app/bloc/app_event.dart
@@ -0,0 +1,13 @@
+part of 'app_bloc.dart';
+
+sealed class AppEvent {
+  const AppEvent();
+}
+
+final class _AppUserChanged extends AppEvent {
+  const _AppUserChanged(this.user);
+
+  final User user;
+}
+
+final class AppLogoutRequested extends AppEvent {}

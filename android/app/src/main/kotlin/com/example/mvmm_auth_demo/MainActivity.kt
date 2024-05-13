9b664fd331eb0e5e856b6e1367714a5da32d0e44
yunwe <sawyunwe.ucsy@gmail.com>
2024-05-13 14:35:58 +0700

    recreated android folder

--- android/app/src/main/kotlin/com/example/mvmm_auth_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/mvmm_auth_demo/MainActivity.kt
@@ -2,4 +2,5 @@ package com.example.mvmm_auth_demo
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity()
+class MainActivity: FlutterActivity() {
+}
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_plus
-import device_info_plus
+import firebase_auth
+import firebase_core
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
